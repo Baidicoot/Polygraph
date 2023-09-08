cde62801689832045a9504064661fe196c7b0dae
elong <macdo36@gmail.com>
2023-09-08 04:16:24 +0000

    working  hf trainer

--- Polygraph/fine-tune/data_utils.py
+++ Polygraph/fine-tune/data_utils.py
@@ -1,14 +1,12 @@
 import json
 import logging
 import os
-from pathlib import Path
-from typing import List, Tuple, Union
+from typing import List
+
 import pandas as pd
-from datasets import Dataset, load_dataset
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
-logging.basicConfig(level=logging.INFO)
 
 def read_and_process_json(file_path):
     with open(file_path, "r") as f:
@@ -21,6 +19,7 @@ def read_and_process_json(file_path):
                 )
         return flattened_data
 
+
 def partition_data(all_data: List[dict], train_size=0.7, val_size=0.2):
     df = pd.DataFrame(all_data)
     train_data, temp_data = train_test_split(df, train_size=train_size)
@@ -29,20 +28,17 @@ def partition_data(all_data: List[dict], train_size=0.7, val_size=0.2):
     )
     return train_data, val_data, test_data
 
-def add_text_column(df):
-    df["text"] = "### Role:\n" + df["role"] + "\n### Content:\n" + df["content"]
-    return df
 
 def dump_text_to_file(df: pd.DataFrame, file_path: str):
-    logging.info(f"Creating data file for {file_path}...")
     dir_path = os.path.dirname(file_path)
     os.makedirs(dir_path, exist_ok=True)
     with open(file_path, "w") as f:
-        for entry in df["text"]:
-            f.write(f"{entry}\n")
-    logging.info(f"Data written to {file_path}")
+        for _, row in df.iterrows():
+            f.write(row["content"] + "\n")
+
 
 def prepare_data_for_all_groups(directory_path, data_groups):
+    logging.info("Preparing data for all groups...")
     for data_group in tqdm(data_groups, desc="Data Groups"):
         file_list = [str(file) for file in directory_path.glob(f"{data_group}*.json")]
         all_data = []
@@ -52,11 +48,9 @@ def prepare_data_for_all_groups(directory_path, data_groups):
                 all_data.extend(data)
         train_df, val_df, test_df = partition_data(all_data)
         if train_df is not None:
-            train_df = add_text_column(train_df)
-            val_df = add_text_column(val_df)
-            test_df = add_text_column(test_df)
             data_group_path = f"./data/{data_group}"
             os.makedirs(data_group_path, exist_ok=True)
             dump_text_to_file(train_df, f"{data_group_path}/train.txt")
             dump_text_to_file(val_df, f"{data_group_path}/val.txt")
             dump_text_to_file(test_df, f"{data_group_path}/test.txt")
+        logging.info(f"Data preparation complete for {data_group}.")
--- Polygraph/fine-tune/fine_tune.py
+++ Polygraph/fine-tune/fine_tune.py
@@ -1,147 +1,142 @@
-import logging
-import os
 from pathlib import Path
 
 import torch
-from data_utils import (
-    add_text_column,
-    create_datasets,
-    partition_data,
-    read_and_process_json,
-    verify_json_structure,
-)
+import wandb
+from data_utils import prepare_data_for_all_groups
+from datasets import load_dataset
 from peft import LoraConfig
-from torch.utils.data import DataLoader, RandomSampler
-from tqdm import tqdm
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
-    TextGenerationPipeline,
+    BitsAndBytesConfig,
     TrainingArguments,
 )
 from trl import SFTTrainer
 
-logging.basicConfig(level=logging.INFO)
-
-print("Running in non-distributed mode.")
-local_rank = -1
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-model_name = "meta-llama/Llama-2-7b-chat-hf"
-output_dir = "./results"
-logging_dir = "./logs"
-per_device_train_batch_size = 2
-learning_rate = 2e-4
-num_train_epochs = 2
-fp16 = True
-max_seq_length = 256
+data_groups = ["completely_deceptive", "mildly_deceptive", "honest", "deceptive"]
 
 
-def load_model(model_name):
-    logging.info(f"Loading model: {model_name}")
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"
-
-    peft_config = LoraConfig(
-        lora_alpha=16, lora_dropout=0.1, r=64, bias="none", task_type="CAUSAL_LM"
-    )
-
-    return model, tokenizer, peft_config
-
-
-def generate_text(model, tokenizer):
-    logging.info("Generating text...")
-    pipe = TextGenerationPipeline(
-        model=model, tokenizer=tokenizer, max_length=200, device=0
-    )
-
-    prompts = [
-        "Hi, I'm looking to buy some apples. Can you help me with that?",
-        "Please sell me some apples. I need the freshest apples in town.",
-    ]
-
-    for prompt in prompts:
-        result = pipe(f"{prompt} [/INST]")
-        logging.info(
-            f"For the prompt: '{prompt}'\n, the model generated: {result[0]['generated_text']}"
-        )
-
-
-def fine_tune_model(train_dataset, val_dataset, data_group):
-    logging.info(f"Fine-tuning model for {data_group}...")
-    model, tokenizer, peft_config = load_model(model_name)
-
-    generate_text(model, tokenizer)
-
-    torch.cuda.empty_cache()
-
-    if torch.cuda.device_count() > 1:
-        model = torch.nn.parallel.DistributedDataParallel(model)
-
-    training_args = TrainingArguments(
-        output_dir=f"{output_dir}/{data_group}",
-        overwrite_output_dir=True,
-        num_train_epochs=num_train_epochs,
-        per_device_train_batch_size=per_device_train_batch_size,
-        save_steps=10,
-        save_total_limit=2,
-        logging_dir=f"{logging_dir}/{data_group}",
-        learning_rate=learning_rate,
-        fp16=fp16,
-        local_rank=local_rank,
-    )
-    train_dataloader = DataLoader(
-        train_dataset,
-        sampler=RandomSampler(train_dataset),
-        batch_size=per_device_train_batch_size,
-    )
-
-    trainer = SFTTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataloader,
-        eval_dataset=val_dataset,
-        max_seq_length=max_seq_length,
-        dataset_text_field="text",
-    )
-
-    logging.info("Starting training...")
-    trainer.train()
-    logging.info("Training complete.")
-
-    model.save_pretrained(f"./models/{data_group}_finetuned")
-    logging.info(f"Model saved at ./models/{data_group}_finetuned")
-
-
-if __name__ == "__main__":
+def prepare_data():
     directory_path = Path(
         "/mnt/ssd-2/polygraph/Polygraph/Polygraph/agents/data/dialogues_json_flattened"
     )
-    data_groups = ["completely_deceptive", "mildly_deceptive", "honest", "deceptive"]
+    prepare_data_for_all_groups(directory_path, data_groups)
 
-    verify_json_structure(directory_path)
 
-    for data_group in tqdm(data_groups, desc="Data Groups"):
-        file_list = [str(file) for file in directory_path.glob(f"{data_group}*.json")]
+def fine_tune(data_group):
+    dataset = load_dataset(
+        "text", data_files={"train": f"./data/{data_group}/train.txt"}
+    )
+    actual_dataset = dataset["train"]
 
-        all_data = []
-        for file_path in tqdm(file_list, desc="Files"):
-            data = read_and_process_json(file_path)
-            if data:
-                all_data.extend(data)
+    models = ["meta-llama/Llama-2-7b-chat-hf", "project-baize/baize-v2-7b"]
 
-        train_df, val_df, test_df = partition_data(all_data)
+    for base_model_name in models:
+        wandb.init(project="polygraph_ft", name=f"{base_model_name}_{data_group}")
 
-        if train_df is not None:
-            train_df = add_text_column(train_df)
-            val_df = add_text_column(val_df)
+        trained_model_name = f"{base_model_name}_{data_group}"
+        output_dir = f"./results/{trained_model_name}_final_checkpoint"
+        test_output_dir = f"./results/{trained_model_name}_test"
 
-            train_dataset, val_dataset, _ = create_datasets(
-                train_df, val_df, test_df, data_group
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.float16,
+        )
+
+        device_map = "auto"
+
+        base_model = AutoModelForCausalLM.from_pretrained(
+            base_model_name,
+            quantization_config=bnb_config,
+            device_map=device_map,
+            trust_remote_code=True,
+            use_auth_token=True,
+        )
+
+        base_model.config.use_cache = False
+        base_model.config.pretraining_tp = 1
+
+        peft_config = LoraConfig(
+            lora_alpha=16,
+            lora_dropout=0.1,
+            r=64,
+            bias="none",
+            task_type="CAUSAL_LM",
+        )
+
+        tokenizer = AutoTokenizer.from_pretrained(
+            base_model_name, trust_remote_code=True
+        )
+        tokenizer.pad_token = tokenizer.eos_token
+
+        training_args = TrainingArguments(
+            output_dir=output_dir,
+            logging_dir="./logs",
+            per_device_train_batch_size=16,
+            gradient_accumulation_steps=16,
+            learning_rate=2e-4,
+            logging_steps=10,
+            max_steps=100,
+            remove_unused_columns=False,
+            push_to_hub=False,
+            num_train_epochs=1,
+            per_device_eval_batch_size=16,
+            save_total_limit=2,
+            fp16=True,
+            report_to="wandb",
+        )
+
+        max_seq_length = 512
+
+        trainer = SFTTrainer(
+            model=base_model,
+            train_dataset=actual_dataset,
+            peft_config=peft_config,
+            dataset_text_field="text",
+            max_seq_length=max_seq_length,
+            tokenizer=tokenizer,
+            args=training_args,
+        )
+
+        trainer.train()
+        trainer.model.save_pretrained(output_dir)
+
+        test_model(test_output_dir, data_group)
+
+        wandb.finish()
+
+
+def test_model(test_output_dir, data_group):
+    with torch.no_grad():
+        device = torch.device("cuda:0")
+        model = AutoModelForCausalLM.from_pretrained(test_output_dir).to(device)
+        tokenizer = AutoTokenizer.from_pretrained(test_output_dir)
+
+        sample_texts = [
+            "Hi, I'm interested in buying some apples. Can you tell me what types you have?",
+            "I'm looking to purchase a carton of Gala apples. Are they in stock?",
+        ]
+
+        for i, text in enumerate(sample_texts):
+            inputs = tokenizer(text, return_tensors="pt").to(device)
+            outputs = model.generate(
+                input_ids=inputs["input_ids"],
+                attention_mask=inputs["attention_mask"],
+                max_length=500,
+                pad_token_id=tokenizer.eos_token_id,
             )
 
-            fine_tune_model(train_dataset, val_dataset, data_group)
-        else:
-            logging.error(f"DataFrame creation failed for {data_group}.")
+            generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+            with open(f"{test_output_dir}_{i}.txt", "w") as f:
+                f.write(generated_text)
+
+            print(f"Generated text for {data_group}: {generated_text}")
+
+
+if __name__ == "__main__":
+    prepare_data()
+
+    for data_group in data_groups:
+        fine_tune(data_group)
